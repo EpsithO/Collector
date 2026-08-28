64bb5c8b774cac7db625abbd869e8d9ed729bc6b
EpsithO <matheodu25@gmail.com>
2026-08-28 19:06:22 +0200

    service C++ conteneurisé, non-root, avec un healthcheck fonctionnel

--- services/catalogue/src/main.cpp
+++ services/catalogue/src/main.cpp
@@ -1,3 +1,28 @@
 //
 // Created by matheo on 28/08/2026.
 //
+#include <drogon/drogon.h>
+
+int main()
+{
+    drogon::app().registerHandler(
+        "/health",
+        [](const drogon::HttpRequestPtr &,
+           std::function<void(const drogon::HttpResponsePtr &)> &&callback)
+        {
+            Json::Value body;
+            body["status"] = "ok";
+            body["service"] = "catalogue";
+
+            auto resp = drogon::HttpResponse::newHttpJsonResponse(body);
+            callback(resp);
+        },
+        {drogon::Get});
+
+    LOG_INFO << "catalogue démarre sur 0.0.0.0:8080";
+
+    drogon::app()
+        .addListener("0.0.0.0", 8080)
+        .setThreadNum(0)
+        .run();
+}
